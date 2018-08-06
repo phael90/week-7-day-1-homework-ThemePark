f29f9617197a5a6b30db0d340185cbd35597dab6
Raphael Ugha <raphaelugha@gmail.com>
2018-08-06 23:45:54 +0100

    In class TobaccoStall added defaultPrice function, tested.

--- src/main/java/Stalls/TobaccoStall.java
+++ src/main/java/Stalls/TobaccoStall.java
@@ -3,16 +3,21 @@ package Stalls;
 import Interfaces.ISecurity;
 import Visitors.Visitor;
 
-public class TobaccoStall  extends Stall implements ISecurity {
-    public TobaccoStall(String name, String ownerName, int parkingSpot){
+public class TobaccoStall extends Stall implements ISecurity {
+    public TobaccoStall(String name, String ownerName, int parkingSpot) {
         super(name, ownerName, parkingSpot);
     }
+
     public boolean isAllowedTo(Visitor visitor) {
         if (visitor.getAge() <= 18) {
             return true;
         } else {
             return false;
         }
+    }
+
+    public double defaultPrice() {
+        return 2.80;
     }
 
 }
