ecc4fc79be0de65f820e16d7ca46f8d45a452ecb
Raphael Ugha <raphaelugha@gmail.com>
2018-08-06 21:46:50 +0100

    Tested CandyFloss for name, ownerName and parkingSpot

--- src/main/java/Stalls/CandyFloss.java
+++ src/main/java/Stalls/CandyFloss.java
@@ -4,4 +4,5 @@ public class CandyFloss extends Stall{
     public CandyFloss(String name, String ownerName, int parkingSpot){
         super(name, ownerName, parkingSpot);
     }
+
 }
--- src/test/java/CandyStallTest.java
+++ src/test/java/CandyStallTest.java
@@ -1,2 +1,29 @@
+import Stalls.CandyFloss;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CandyStallTest {
+    CandyFloss candyFloss;
+
+    @Before
+    public void before(String name, String ownerName, int parkingSpot){
+        candyFloss = new CandyFloss("CandyLand", "Digory", 2);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("CandyLand", candyFloss.getName());
+    }
+
+    @Test
+    public void hasOwnerName() {
+        assertEquals("Digory", candyFloss.getName());
+    }
+
+    @Test
+    public void hasParkingSpot(){
+        assertEquals(2, candyFloss.getParkingSpot());
+    }
 }
