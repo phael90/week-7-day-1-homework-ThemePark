d41b2d4a161dcec08790fcce51b0fab070039dd6
Raphael Ugha <raphaelugha@gmail.com>
2018-08-06 23:44:10 +0100

    In class CandyStall added defaultPrice function, tested.

--- src/main/java/Stalls/CandyFloss.java
+++ src/main/java/Stalls/CandyFloss.java
@@ -1,8 +1,11 @@
 package Stalls;
 
-public class CandyFloss extends Stall{
-    public CandyFloss(String name, String ownerName, int parkingSpot){
+public class CandyFloss extends Stall {
+    public CandyFloss(String name, String ownerName, int parkingSpot) {
         super(name, ownerName, parkingSpot);
     }
 
+    public double defaultPrice() {
+        return 4.20;
+    }
 }
