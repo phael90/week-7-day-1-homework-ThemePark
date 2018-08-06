f2623928b53a893ba91cf643fe69b031aa8549cd
Raphael Ugha <raphaelugha@gmail.com>
2018-08-06 23:16:19 +0100

    In class TobaccoStall added isAllowedTo function, tested.

--- src/main/java/Stalls/TobaccoStall.java
+++ src/main/java/Stalls/TobaccoStall.java
@@ -1,8 +1,17 @@
 package Stalls;
 
+import Visitors.Visitor;
+
 public class TobaccoStall  extends Stall{
     public TobaccoStall(String name, String ownerName, int parkingSpot){
         super(name, ownerName, parkingSpot);
+    }
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() <= 18) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
