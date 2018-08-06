02e9b55ad711fabf34f2b2ade5aa8ad51481ab36
Raphael Ugha <raphaelugha@gmail.com>
2018-08-07 00:00:33 +0100

    In class Rollercoster  added priceFor function, tested.

--- src/main/java/Attractions/Rollercoster.java
+++ src/main/java/Attractions/Rollercoster.java
@@ -6,20 +6,30 @@ import Visitors.Visitor;
 
 public class Rollercoster extends Attraction implements ISecurity {
 
-    public Rollercoster(String name){
+    public Rollercoster(String name) {
         super(name);
     }
 
     public boolean isAllowedTo(Visitor visitor) {
-        if (visitor.getAge() >= 12 && visitor.getHeight() >= 145){
+        if (visitor.getAge() >= 12 && visitor.getHeight() >= 145) {
             return true;
         } else {
             return false;
         }
     }
-    public double defaultPrice(){
+
+    public double defaultPrice() {
         return 8.40;
     }
 
+    public double priceFor(Visitor visitor){
+        double price = defaultPrice();
+        if (visitor.getHeight() >= 200){
+            price *= 2;
+        } else {
+            price = price;
+        }
+        return price;
+    }
 
 }
