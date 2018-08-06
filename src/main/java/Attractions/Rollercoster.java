e5ac31b11754485da4c094c4c30b4ecd352a22f2
Raphael Ugha <raphaelugha@gmail.com>
2018-08-06 23:22:24 +0100

    In class Rollercoster  added isAllowedTo function, tested.

--- src/main/java/Attractions/Rollercoster.java
+++ src/main/java/Attractions/Rollercoster.java
@@ -1,8 +1,18 @@
 package Attractions;
 
+import Visitors.Visitor;
+
 public class Rollercoster extends Attraction{
 
     public Rollercoster(String name){
         super(name);
+    }
+
+    public boolean isAllowedTo(Visitor visitor) {
+        if (visitor.getAge() >= 12 && visitor.getHeight() >= 145){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
